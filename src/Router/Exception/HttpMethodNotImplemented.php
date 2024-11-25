cbfb4fc70377ff7f1672737af32727714c57b078
iya-kho <iya.kho@gmail.com>
2024-11-25 19:56:06 +0200

    \Exception added

--- src/Router/Exception/HttpMethodNotImplemented.php
+++ src/Router/Exception/HttpMethodNotImplemented.php
@@ -4,7 +4,7 @@ namespace Framework312\Router\Exception;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class HttpMethodNotImplemented extends Exception {
+class HttpMethodNotImplemented extends \Exception {
     public function __construct(string $classname, string $method) {
         parent::__construct("Method {$method} not implemented for class {$classname}", Response::HTTP_METHOD_NOT_ALLOWED);
     }
--- src/Router/Exception/InvalidViewImplementation.php
+++ src/Router/Exception/InvalidViewImplementation.php
@@ -4,7 +4,7 @@ namespace Framework312\Router\Exception;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class InvalidViewImplementation extends Exception {
+class InvalidViewImplementation extends \Exception {
     public function __construct(string $classname) {
         parent::__construct("Class {$classname} does not extend Framework312\Router\View\BaseView class, cannot be used as a Router's view.", Response::HTTP_INTERNAL_SERVER_ERROR);
     }
